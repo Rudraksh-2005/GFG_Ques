f98719e8b1cf2c3fa69c230274b9ad6398026179
Rudraksh Agrawal <144117271+Rudraksh-2005@users.noreply.github.com>
2026-09-04 10:42:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bird and Max Fruit Gathering/bird-and-max-fruit-gathering.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maxFruits(vector<int>& arr, int m) {
+        // code here
+       
+       int n = arr.size();
+       int i = 0, j = 0;
+       long long res = -1;
+       long long sum = 0;
+
+       while (m--)
+           sum += arr[j++];
+       j--;
+
+       res = sum;
+
+       while (i < arr.size()) {
+           sum += (arr[(++j) % n] - arr[i++]);
+           res = max(res, sum);
+       }
+
+       return res;
+        
+    }
+};
